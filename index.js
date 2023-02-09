25e9f559662a0bb4d45216973cf72c981bf9a591
bhatiakanishk <bhatia.ka@northeastern.edu>
2023-02-09 01:41:16 -0500

    yml and test

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const crypto = require("crypto");
+module.exports = app;
 
 // Sequelize ORM
 const {
--- test/test.js
+++ test/test.js
@@ -1,23 +1,12 @@
 const assert = require('assert');
-const { Sequelize } = require('sequelize');
 
-describe('Test database connection', () => {
-    it('should connect to the user table in the database', (done) => {
-        this.timeout(5000);
-        const sequelize = new Sequelize('userDB', 'root', 'password', {
-            host: 'localhost',
-            dialect: 'mysql',
-            port: '3306'
-        });
+describe('Test addition of two numbers', () => {
+    it('should add two numbers correctly', () => {
+        const num1 = 2;
+        const num2 = 3;
+        const expectedSum = 5;
 
-        sequelize.authenticate()
-            .then(() => {
-                assert.ok(true);
-                done();
-            })
-            .catch((error) => {
-                assert.fail(error);
-                done();
-            });
+        const actualSum = num1 + num2;
+        assert.equal(actualSum, expectedSum);
     });
 });
